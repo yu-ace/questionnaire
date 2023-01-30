04bce43ca0c843145c399195705402e80646f751
admin <18706110899@163.com>
2023-01-31 01:03:53 +0800

    创建

---
+++ src/main/java/com/example/questionnaire/model/Questionnaire.java
@@ -0,0 +1,54 @@
+package com.example.questionnaire.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "questionnaire")
+@DynamicUpdate
+public class Questionnaire {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Integer id;
+    @Column(name = "name")
+    String name;
+    @Column(name = "creator_id")
+    Integer creatorId;
+    @Column(name = "create_time")
+    Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(Integer creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
